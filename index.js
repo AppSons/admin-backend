bda53b083acf8c9265e3e898d0303d105c8ad378
Fernando Nielson <fernielson@gmail.com>
2022-02-02 13:37:30 +0100

    SPA Control

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
 const express = require('express');
 require('dotenv').config();
+const path = require('path');
+
 const cors = require('cors');
 
 
 const {dbConnection} = require('./database/config');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 
 //Crear el Servidor de express
 
@@ -35,6 +39,12 @@ app.use('/api/medicos', require('./routes/medicos-routes'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads-r'));
 app.use('/api/login', require('./routes/auth-routes'));
+
+//lo último, si no es cualquiera de las rutas anteriores
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 
 
